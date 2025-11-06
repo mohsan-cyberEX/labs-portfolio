cd9db8126246db2e9f8d0865fcdd13cd22fa6bba
Mohsan | CyberEX Security <mohsansec@gmail.com>
2025-11-07 03:41:06 +0500

    script.js

--- docs/script.js
+++
@@ -1,8 +0,0 @@
-// Smooth scroll for nav links
-document.querySelectorAll('.nav-links a').forEach(link => {
-    link.addEventListener('click', e => {
-        e.preventDefault();
-        const target = document.querySelector(link.getAttribute('href'));
-        target.scrollIntoView({ behavior: 'smooth' });
-    });
-});
